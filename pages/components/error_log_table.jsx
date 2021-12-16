73c33266ee8f2f7173816989f8868fd657124cc4
Matias Espina <matiasespina1991@gmail.com>
2021-12-16 12:20:54 -0300

    fix

---
+++ pages/components/error_log_table.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import axios from 'axios';
+
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+        console.log(persons)
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        {/* { this.state.persons.map((person, index) => <li key={index}>{person.name}</li>)} */}
+      </ul>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import ErrorLogTable from './components/error_log_table'
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-       Hi
+      <ErrorLogTable />
       </main>
 
       <footer className={styles.footer}>
