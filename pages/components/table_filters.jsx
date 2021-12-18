996e15bd5c79368f320d14826f2d5e2537a5846b
Matias Espina <matiasespina1991@gmail.com>
2021-12-18 13:12:42 -0300

    style added to filters

--- pages/components/table_filters.jsx
+++ pages/components/table_filters.jsx
@@ -2,17 +2,21 @@ export default function TableFilters({resultsLimitValue , userIdValue}) {
 
     return(
         <>
-        <div className="table-filters--wrapper">
-            <p>Number of results: </p>
-            <select onChange={resultsLimitValue} defaultValue={'20'} name="limit-select">
-                <option value="5">5</option>
-                <option value="10">10</option>
-                <option value="15">15</option>
-                <option value="20">20</option>
-                <option value="25">25</option>
-            </select>
-            <p>User ID: </p>
-            <input onChange={userIdValue} type="text" id="user-id" name="user-id" />
+        <div className="table-filters--container">
+            <div className="filter">
+                <p>Number of results: </p>
+                <select onChange={resultsLimitValue} defaultValue={'20'} name="limit-select">
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                    <option value="25">25</option>
+                </select>
+            </div>
+            <div className="filter">
+                <p>User ID: </p>
+                <input onChange={userIdValue} type="text" id="user-id" name="user-id" />
+            </div>
         </div>
         </>
     )
